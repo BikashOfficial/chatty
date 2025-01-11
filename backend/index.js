001f97bbceca6438f729a55e28255ced4a78c849
Bikash Meher <153185575+BikashOfficial@users.noreply.github.com>
2025-01-12 02:41:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -65,7 +65,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatty-gu8o.onrender.com",
     credentials: true,
   })
 );
